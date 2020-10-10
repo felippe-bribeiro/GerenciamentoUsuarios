b92b219e99d412541d7cd5210d9e8ffc02c4d4aa
unknown <lipibarros@gmail.com>
2020-10-10 01:07:42 -0300

    Add Events

--- index.js
+++ index.js
@@ -5,24 +5,33 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach(function(field, index){
 
-    if(field.name == "gender") {
-
-        if(field.checked) {
-            user.gender = field.value;  
-
-        }
-        
-
-    } else {
-        user[field.name] = field.nodeValue;
-    }
-    //console.log(field);
-    //console.log(index);
-    //console.log(field.name);
-    //console.log(field.id, field.name, field.value, index);
-
-});
 
 console.log(user);
+
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+    event.preventDefault();
+    
+    
+    fields.forEach(function(field, index){
+
+        if(field.name == "gender") {
+    
+            if(field.checked) {
+                user.gender = field.value;  
+    
+            }
+            
+    
+        } else {
+            user[field.name] = field.nodeValue;
+        }
+        //console.log(field);
+        //console.log(index);
+        //console.log(field.name);
+        //console.log(field.id, field.name, field.value, index);
+    
+    });
+
+    
+});
